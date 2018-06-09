f5eba6841bcfb3b8375298f033f20eb94eb2522a
hyunji <hyunjm95@naver.com>
2018-06-09 18:01:21 +0900

    merge ver1

--- src/main/java/com/watchers/model/RawData.java
+++ src/main/java/com/watchers/model/RawData.java
@@ -13,8 +13,144 @@ public class RawData {
 	private Long ankleleft;
 	private Long footleft;
 	private Long footright;
+	private Long handtipright;
+	private Long handtipleft;
+	private Long handright;
+	private Long handleft;
+	private Long thumbright;
+	private Long thumbleft;
+	private Long wristright;
+	private Long wristleft;
+	private Long elbowleft;
+	private Long elbowright;
+	private Long shoulderright;
+	private Long shoulderleft;
+	private Long spineshoulder;
+	private Long neck;
+	private Long head;
+
 	private Timestamp date;
-	
+
+	public Long getHandtipright() {
+		return handtipright;
+	}
+
+	public void setHandtipright(Long handtipright) {
+		this.handtipright = handtipright;
+	}
+
+	public Long getHandtipleft() {
+		return handtipleft;
+	}
+
+	public void setHandtipleft(Long handtipleft) {
+		this.handtipleft = handtipleft;
+	}
+
+	public Long getHandright() {
+		return handright;
+	}
+
+	public void setHandright(Long handright) {
+		this.handright = handright;
+	}
+
+	public Long getHandleft() {
+		return handleft;
+	}
+
+	public void setHandleft(Long handleft) {
+		this.handleft = handleft;
+	}
+
+	public Long getThumbright() {
+		return thumbright;
+	}
+
+	public void setThumbright(Long thumbright) {
+		this.thumbright = thumbright;
+	}
+
+	public Long getThumbleft() {
+		return thumbleft;
+	}
+
+	public void setThumbleft(Long thumbleft) {
+		this.thumbleft = thumbleft;
+	}
+
+	public Long getWristright() {
+		return wristright;
+	}
+
+	public void setWristright(Long wristright) {
+		this.wristright = wristright;
+	}
+
+	public Long getWristleft() {
+		return wristleft;
+	}
+
+	public void setWristleft(Long wristleft) {
+		this.wristleft = wristleft;
+	}
+
+	public Long getElbowleft() {
+		return elbowleft;
+	}
+
+	public void setElbowleft(Long elbowleft) {
+		this.elbowleft = elbowleft;
+	}
+
+	public Long getElbowright() {
+		return elbowright;
+	}
+
+	public void setElbowright(Long elbowright) {
+		this.elbowright = elbowright;
+	}
+
+	public Long getShoulderright() {
+		return shoulderright;
+	}
+
+	public void setShoulderright(Long shoulderright) {
+		this.shoulderright = shoulderright;
+	}
+
+	public Long getShoulderleft() {
+		return shoulderleft;
+	}
+
+	public void setShoulderleft(Long shoulderleft) {
+		this.shoulderleft = shoulderleft;
+	}
+
+	public Long getSpineshoulder() {
+		return spineshoulder;
+	}
+
+	public void setSpineshoulder(Long spineshoulder) {
+		this.spineshoulder = spineshoulder;
+	}
+
+	public Long getNeck() {
+		return neck;
+	}
+
+	public void setNeck(Long neck) {
+		this.neck = neck;
+	}
+
+	public Long getHead() {
+		return head;
+	}
+
+	public void setHead(Long head) {
+		this.head = head;
+	}
+
 	public Long getId() {
 		return id;
 	}
